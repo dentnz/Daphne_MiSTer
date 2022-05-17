7ceea352ca38ab98c6bff21e67a47a9de4a64a98
Karl Lurman <karl.lurman@wilsongroupau.com>
2022-05-17 10:32:24 +1000

    Add z80 core. Update readme. Unable to verilate the mpeg2fpga currently

--- README.md
+++ README.md
@@ -7,10 +7,12 @@ A MiSTer core capable of running FMV games like Dragon's Lair.
 https://projectf.io/posts/verilog-sim-verilator-sdl/
 
 ### Mpeg2 Decoder
-Still not 100% certain this is required. Possible candidate(s):
-https://www.ece.mcmaster.ca/~nicola/mpeg.html
+
+Daphne compatible ROMS seem to all use mpeg2. MPEG2FPGA open core seems to be best.
 
 https://opencores.org/websvn/listing?repname=mpeg2fpga&path=&
+
+https://www.ece.mcmaster.ca/~nicola/mpeg.html
 
 ### COPS420/421 - Not sourced yet
 Seems to be a MCU present in the Apple Lisa. Unable to source RTL for this component at this stage.
@@ -24,4 +26,16 @@ Should use a verilog implementation for this
 Verilog found in this repo. Added to RTL folder as a git submodule
 
 https://github.com/cavnex/mc6809
+
+### Z80 - Sourced
+Used in Lair.
+
+https://github.com/Time0o/z80-verilog.git
+
+## Game info and analysis
+
+### Dragons Lair
+
+Schematic/owners manual:
+http://www.dragons-lair-project.com/tech/schematics/Dragons_Lair_Schematic_Package_Supplement_Europe.pdf
 
--- sim/top_daphne.sv
+++ sim/top_daphne.sv
@@ -45,4 +45,49 @@ module top_daphne #(parameter CORDW=10) (  // coordinate width
         sdl_g <= {2{paint_g}};
         sdl_b <= {2{paint_b}};
     end
+
+
+/*
+ * mpeg2 decoder
+ */
+
+    mpeg2video mpeg2 (
+    .clk(clk),
+    .mem_clk(mem_clk),
+    .dot_clk(dot_clk),
+    .rst(rst),
+    .stream_data(stream_data),
+    .stream_valid(stream_valid),
+    .reg_addr(reg_addr),
+    .reg_wr_en(reg_wr_en),
+    .reg_dta_in(reg_dta_in),
+    .reg_rd_en(reg_rd_en),
+    .reg_dta_out(reg_dta_out),
+    .busy(busy),
+    .error(error),
+    .interrupt(interrupt),
+    .watchdog_rst(),
+    .r(r),
+    .g(g),
+    .b(b),
+    .y(y),
+    .u(u),
+    .v(v),
+    .pixel_en(pixel_en),
+    .h_sync(h_sync),
+    .v_sync(v_sync),
+    .c_sync(),
+    .mem_req_rd_cmd(mem_req_rd_cmd),
+    .mem_req_rd_addr(mem_req_rd_addr),
+    .mem_req_rd_dta(mem_req_rd_dta),
+    .mem_req_rd_en(mem_req_rd_en),
+    .mem_req_rd_valid(mem_req_rd_valid),
+    .mem_res_wr_dta(mem_res_wr_dta),
+    .mem_res_wr_en(mem_res_wr_en),
+    .mem_res_wr_almost_full(mem_res_wr_almost_full),
+    .testpoint(testpoint),
+    .testpoint_dip(4'h0),
+    .testpoint_dip_en(1'b1)
+    );
+
 endmodule
